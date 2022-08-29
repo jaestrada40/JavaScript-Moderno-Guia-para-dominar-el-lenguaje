7c9c4400b9428ae1606f17a995cd1e05517e33ab
jaestrada40 <43049997+jaestrada40@users.noreply.github.com>
2022-08-29 16:06:22 -0600

    javascript ejercicio 1

---
+++ 01-FUNDAMENTOS/assets/js/alerts.js
@@ -0,0 +1,10 @@
+
+
+alert('Hola mundo')
+
+let nombre = prompt('Cual es tu nombre');
+console.log(nombre);
+console.log('***' + nombre + '***');
+
+const seleccion =  confirm('Are you sure you want to continue');
+console.log(seleccion);
---
+++ 01-FUNDAMENTOS/assets/js/app.js
@@ -0,0 +1,14 @@
+
+
+let a= 10,
+    b= 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c +d;
+
+
+c = 'Hola de nuevo';
+
+var miNombre = 'Javier';
