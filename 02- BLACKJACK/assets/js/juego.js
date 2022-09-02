f87307357b18fde041ff83678ebaf44fdd5cb005
jaestrada40 <43049997+jaestrada40@users.noreply.github.com>
2022-09-02 15:53:20 -0600

    Poniendo putnos a las cartas.

--- 02- BLACKJACK/assets/js/juego.js
+++ 02- BLACKJACK/assets/js/juego.js
@@ -10,6 +10,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S']
 const especiales = ['A','J', 'Q','K']
 
+// Esta función crea un nuevo deck
 const crearDeck = () =>{
 
     for( let i = 2; i<= 10; i++){
@@ -23,7 +24,47 @@ const crearDeck = () =>{
             deck.push( esp + tipo );
         }
     }
+    // console.log(deck);
+    deck = _.shuffle(deck);
     console.log(deck);
+    return deck;
 }
 
 crearDeck();
+
+
+// Esta función me permite tomar una carta
+const pedirCarta = () =>{
+
+    if ( deck.length === 0){
+        throw 'No hay cartas en el deck';
+    }
+
+    const carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta);
+    return  carta;
+}
+
+
+//pedirCarta();
+
+const valorCarta = ( carta ) =>{
+
+    const valor = carta.substring(0, carta.length -1);
+    let puntos = 0;
+    // 2 = 2, 10 = 10, 3 = 3
+    if( isNaN( valor ) ){
+
+        puntos = ( valor === 'A' ) ? 11: 10;
+
+    }else{
+
+        puntos = valor * 1;
+    }
+    console.log(puntos);
+}
+
+valorCarta('9D');
+
